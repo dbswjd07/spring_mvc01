eb5483a52b5a6dbb0d271332eeb30c9acffc6a69
dbswjd07 <10823syn@naver.com>
2023-08-02 13:56:31 +0900

    spring day12

---
+++ src/main/java/net/daum/controller/SampleController.java
@@ -0,0 +1,29 @@
+package net.daum.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/sample/*") //컨트롤 자체 매핑주소 등록
+public class SampleController {
+
+	@GetMapping("/all") //로그인 하지 않은 사용자도 접근 가능한 매핑주소
+	public void doAll() {
+		/*
+		 * 리턴타입이 없는 void 형 아기 때문에 매핑주소가 뷰페이지 파일명이 된다.
+		 * 뷰리졸브 경로는 /WEB-INF/views/sample/all.jsp
+		 */
+		System.out.println("do all can access everyBody");
+	}
+	
+	@GetMapping("/member") //로그인 한 사용자만 접근 가능한 매핑주소
+	public void doMember() {
+		System.out.println("logined member");
+	}
+	
+	@GetMapping("/admin") //로그인 한 사용자들 중에서 관리자 권한을 가진 사용자만 접근 가능한 매핑주소
+	public void doAdmin() {
+		System.out.println("admin only");
+	}
+}
